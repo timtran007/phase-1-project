0f5cdbe8d92b558026b06ce8ebcc381e5f2c0011
Tim <timtran007@gmail.com>
2022-10-15 12:01:57 -0500

    added append feature after searching for city and/or state

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+let unorderedListLocation = document.querySelector('#recall-list')
+    
+//DOM Content Loaded functions
 document.addEventListener('DOMContentLoaded', (e) => {
     fetchOngoingRecall()
+
 })
-
-
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let searchedCity = document.querySelector('#search-city').value
-    let selectedState = document.querySelector('#state-dropdown').value 
-    populateSearch(searchedCity, selectedState)
-}
-)
 
 function fetchOngoingRecall(){
     fetch ('https://api.fda.gov/food/enforcement.json?limit=1000')
@@ -22,7 +16,6 @@ function fetchOngoingRecall(){
 }
 
 function loadOngoingRecall(products){
-    let unorderedListLocation = document.querySelector('#recall-list')
     let headerOfLoadedContent = document.createElement('div')
     headerOfLoadedContent.innerHTML = `
     <h2 id="loaded-product-header">Ongoing Products On Recall</h2>
@@ -46,16 +39,76 @@ function loadOngoingRecall(products){
     })
 }
 
-function populateSearch(city, state){
-    fetchOngoingRecallForSearch()
+//Search Event Listener
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let searchedCity = document.querySelector('#search-city').value
+    let selectedState = document.querySelector('#state-dropdown').value 
+    populateSearch(searchedCity, selectedState)
+}
+)
 
+function populateSearch(city, state){
+    fetchOngoingRecallForSearch(city, state)
 }
 
 
-function fetchOngoingRecallForSearch(){
+function fetchOngoingRecallForSearch(city, state){
     fetch ('https://api.fda.gov/food/enforcement.json?limit=1000')
     .then(resp => resp.json())
     .then(data => {
         console.log(data)
+        removeAllChildNodes(document.querySelector('#recall-list'))
+        loadSearch(data, city, state)    
     })
 }
+
+function loadSearch(products, city, state){
+    let headerOfLoadedContent = document.createElement('div')
+    headerOfLoadedContent.innerHTML = `
+    <h2 id="loaded-product-header">${state}'s Products On Recall</h2>
+    `
+    unorderedListLocation.append(headerOfLoadedContent)
+    products.results.forEach(product => {
+        if(product.city === city && product.state === state && product.status ==="Ongoing"){
+            console.log(product.city, product.state)
+            let searchedProductCityAndState = document.createElement('li')
+            searchedProductCityAndState.className = 'card'
+            searchedProductCityAndState.innerHTML = `
+                <h3>Product Description: ${product.product_description}</h3>
+                <h4>Date of Recall: ${product.recall_initiation_date} </h4>
+                <h4> Status of Recall: ${product.status}</h4>
+                <p> Location: ${product.city}, ${product.state}</p>
+                <p> Product Type: ${product.product_type}</p>
+                <p> Quantities Affected: ${product.product_quantity}</p>
+                <p> Lot Codes & Exp Date: ${product.code_info}</p>
+                <input type="submit" value="Remove from List"/>
+            `
+            unorderedListLocation.append(searchedProductCityAndState)
+        } else if(product.state === state && product.status ==="Ongoing"){
+            console.log(product.state)
+            let searchedProductByState = document.createElement('li')
+            searchedProductByState.className = 'card'
+            searchedProductByState.innerHTML = `
+                <h3>Product Description: ${product.product_description}</h3>
+                <h4>Date of Recall: ${product.recall_initiation_date} </h4>
+                <h4> Status of Recall: ${product.status}</h4>
+                <p> Location: ${product.city}, ${product.state}</p>
+                <p> Product Type: ${product.product_type}</p>
+                <p> Quantities Affected: ${product.product_quantity}</p>
+                <p> Lot Codes & Exp Date: ${product.code_info}</p>
+                <input type="submit" value="Remove from List"/>
+            `
+            unorderedListLocation.append(searchedProductByState)
+        }
+    })
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Adding Click to Remove 
