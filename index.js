1412ae42cc5661da81ebe83fbb64946974f7e594
Tim <timtran007@gmail.com>
2022-10-15 00:50:48 -0500

    created fetch funcdtion + appending when content loads & made adjustments to index.html to fix script placement

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+    fetchOngoingRecall()
+})
+
+function fetchOngoingRecall(){
+    fetch ('https://api.fda.gov/food/enforcement.json?limit=1000')
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        loadOngoingRecall(data)
+    })
+}
+
+function loadOngoingRecall(products){
+    products.results.forEach(product => {
+        if (product.status === "Ongoing" && product.country ==="United States"){
+            let recalledProduct = document.createElement('li')
+            recalledProduct.className = 'card'
+            console.log(recalledProduct)
+            recalledProduct.innerHTML = `
+                <h3>Product Description: ${product.product_description}</h3>
+                <h4>Date of Recall: ${product.recall_initiation_date} </h4>
+                <h4> Status of Recall: ${product.status}</h4>
+                <p> Location: ${product.city}, ${product.state}</p>
+                <p> Product Type: ${product.product_type}</p>
+                <p> Quantities Affected: ${product.product_quantity}</p>
+                <p> Lot Codes & Exp Date: ${product.code_info}</p>
+            `
+
+
+            let unorderedListLocation = document.querySelector('#recall-list')
+            unorderedListLocation.appendChild(recalledProduct)
+        }
+    })
+}
